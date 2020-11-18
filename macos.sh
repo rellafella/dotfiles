ce0a3db512ae65857690d310d19bf5d438a8f205
Liam Rella <36829732+rellafella@users.noreply.github.com>
2020-11-19 08:49:57 +1030

    Create macos.sh

---
+++ macos.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+# Show Path Bar in Finder
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Show Status Bar in Finder
+defaults write com.apple.finder ShowStatusBar -bool true
